ef5c7e16292e13eec888a0d834d6afbd4ce85e08
gigazin <danilo_jean05@hotmail.com>
2022-09-26 21:13:25 -0300

    beginning implementation of register screen

---
+++ script.js
@@ -0,0 +1,5 @@
+function showRegisterDiv() {
+  document.getElementById('registerDiv').style.display = 'flex'
+  document.getElementById('mainWrapperDiv').style.filter = 'blur(7px)'
+  document.getElementById('mainWrapperDiv').style.pointerEvents = 'none'
+}
