703a4449515c377ff19039f567003fa82c622de3
gigazin <gigazin@hotmail.com>
2022-09-27 09:01:55 -0300

    importing missing fonts and creating button to close register modal

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function showRegisterDiv() {
   document.getElementById('mainWrapperDiv').style.filter = 'blur(7px)'
   document.getElementById('mainWrapperDiv').style.pointerEvents = 'none'
 }
+
+function hideRegisterDiv() {
+  document.getElementById('registerDiv').style.display = 'none'
+  document.getElementById('mainWrapperDiv').style.filter = 'blur(0px)'
+  document.getElementById('mainWrapperDiv').style.pointerEvents = 'auto'
+}
